23e5a7756375a4d062bece6984438b5779bd0b65
iki1uc <entertrainer@web.de>
2026-07-28 18:13:17 +0200

    Create qmove.js

---
+++ JS/qmove.js
@@ -0,0 +1,11 @@
+// JS/qmove.js
+export function QMOVE() {
+    return [
+        { name: "W",  x: -1, y: 0,  z: 0,  color: "#6cf" },
+        { name: "S",  x: 0,  y: -1, z: 0,  color: "#c66" },
+        { name: "M",  x: 0,  y: 0,  z: 0,  color: "#ccc" },
+        { name: "81", x: -0.5, y: 0.5, z: 1, color: "#fc6" },
+        { name: "Q",  x: 1,  y: 0,  z: 2,  color: "#6f6" },
+        { name: "W",  x: -1, y: 0,  z: 0,  color: "#6cf" }
+    ];
+}
